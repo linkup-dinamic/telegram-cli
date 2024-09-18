fff7f267398fdb6498ee58b6f818b46cb5a8d44b
YuaFox <ruben.hormigo@linkup.cat>
2024-09-18 16:43:30 +0200

    First commit!

---
+++ README.md
@@ -0,0 +1,12 @@
+# telegram-cli
+
+## Build
+
+```
+API_ID=x API_HASH=x ./build.sh
+```
+
+## Install
+
+1. Install the deb file
+2. Edit /opt/telegram-cli/profile.conf and put your phone number
---
+++ build.sh
@@ -0,0 +1,26 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install make git zlib1g-dev libssl-dev gperf php-cli cmake g++
+git clone https://github.com/tdlib/td.git
+cd td
+rm -rf build
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=../tdlib ..
+cmake --build . --target install
+cd ..
+cd ..
+
+cp td/tdlib/lib/libtdjson.so debian-package/opt/telegram-cli/
+
+cat > debian-package/opt/telegram-cli/profile.conf << EOF
+[DEFAULT]
+PHONE_NUMBER=
+API_ID=$(echo $API_ID)
+API_HASH=$(echo $API_HASH)
+EOF
+
+mkdir dist
+dpkg-deb --build debian-package dist
+
+rm debian-package/opt/telegram-cli/profile.conf
